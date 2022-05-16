279bf27e2dc34307e4dfb516d89776e43918e057
Henrik <hreissig@outlook.com>
2022-05-16 12:38:15 +0200

    [FEAT] Add links to the pages in the stats widget

--- resources/js/Widgets/Stats.svelte
+++ resources/js/Widgets/Stats.svelte
@@ -1,6 +1,8 @@
 <script>
     import WidgetContainer from "@/Components/Widgets/WidgetContainer.svelte";
     import Widget from "@/Components/Widgets/Widget.svelte";
+    import {route} from "@/utils";
+    import {inertia} from "@inertiajs/inertia-svelte";
 
     export let stats = {};
 </script>
@@ -8,20 +10,23 @@
 <WidgetContainer>
     <Widget title="Stats" withBackground={false}>
         <dl class="grid grid-cols-3 gap-3 sm:gap-5">
-            <div class="px-4 py-5 bg-primary-100 shadow rounded-lg overflow-hidden sm:p-6">
+            <a href={route('links.index')} use:inertia
+               class="px-4 py-5 bg-primary-100 shadow rounded-lg overflow-hidden sm:p-6">
                 <dt class="text-sm font-medium text-primary-600 truncate">Links</dt>
                 <dd class="mt-1 text-xl font-semibold text-primary-800">{stats.links}</dd>
-            </div>
+            </a>
 
-            <div class="px-4 py-5 bg-primary-100 shadow rounded-lg overflow-hidden sm:p-6">
+            <a href={route('groups.index')} use:inertia
+               class="px-4 py-5 bg-primary-100 shadow rounded-lg overflow-hidden sm:p-6">
                 <dt class="text-sm font-medium text-primary-600 truncate">Groups</dt>
                 <dd class="mt-1 text-xl font-semibold text-primary-800">{stats.groups}</dd>
-            </div>
+            </a>
 
-            <div class="px-4 py-5 bg-primary-100 shadow rounded-lg overflow-hidden sm:p-6">
+            <a href={route('tags.index')} use:inertia
+               class="px-4 py-5 bg-primary-100 shadow rounded-lg overflow-hidden sm:p-6">
                 <dt class="text-sm font-medium text-primary-600 truncate">Tags</dt>
                 <dd class="mt-1 text-xl font-semibold text-primary-800">{stats.tags}</dd>
-            </div>
+            </a>
         </dl>
     </Widget>
 </WidgetContainer>
