90ba9273e7e315558563906cc8eaaada64badd99
Henrik <hreissig@outlook.com>
2022-05-16 12:40:34 +0200

    [FIX] Change the font name to avoid problems with Apache

--- tailwind.config.js
+++ tailwind.config.js
@@ -103,7 +103,7 @@ module.exports = {
         },
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
                 mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
             },
         },
