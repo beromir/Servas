df6a38e94f7c5859448475dd5b41f7c5ee561fbb
Henrik <hreissig@outlook.com>
2022-05-24 20:43:49 +0200

    [TASK] Configure Servas to trust all proxies

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,6 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-window.axios.defaults.baseURL = process.env.APP_URL;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
