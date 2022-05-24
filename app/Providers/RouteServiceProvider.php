b21328a8a20659e71f0f5adff2fa8799b9c4c92d
Henrik <hreissig@outlook.com>
2022-05-24 18:16:36 +0200

    [FEAT] Change home route to '/home'

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
