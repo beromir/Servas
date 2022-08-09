7f0d92c8281403b45b3feffbb2a5cb509faf6af1
Henrik <hreissig@outlook.com>
2022-08-09 19:29:55 +0200

    [FEAT] Make the selection of the setup key and all recovery codes easier

--- resources/js/Pages/Profile/Partials/TwoFactorAuthenticationForm.svelte
+++ resources/js/Pages/Profile/Partials/TwoFactorAuthenticationForm.svelte
@@ -111,7 +111,7 @@
 
                 <div class="mt-4 max-w-xl text-sm text-gray-600">
                     <p class="font-semibold">
-                        Setup Key: {@html setupKey}
+                        Setup Key: <span class="select-all">{@html setupKey}</span>
                     </p>
                 </div>
             {/if}
@@ -124,7 +124,7 @@
                     </p>
                 </div>
 
-                <div class="grid gap-1 max-w-xl mt-4 px-4 py-4 font-mono text-sm bg-gray-100 rounded-lg">
+                <div class="grid gap-1 max-w-xl mt-4 px-4 py-4 font-mono text-sm bg-gray-100 rounded-lg select-all">
                     {#each recoveryCodes as code (code)}
                         {code}<br/>
                     {/each}
