ad504263ee7c9f9f0b4bd5c5f424b4b00afa2537
Henrik <hreissig@outlook.com>
2022-12-11 13:15:15 +0100

    [FEAT] Add CLI command to show Servas stats

---
+++ app/Console/Commands/ShowStats.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Group;
+use App\Models\Link;
+use Illuminate\Console\Command;
+use Spatie\Tags\Tag;
+
+class ShowStats extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'servas:stats';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show Servas stats';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(Link $link, Group $group, Tag $tag)
+    {
+        $this->table(['Model', 'Count'], [
+            ['Links', Link::all()->count()],
+            ['Groups', Group::all()->count()],
+            ['Tags', Tag::all()->count()],
+        ]);
+
+        return Command::SUCCESS;
+    }
+}
