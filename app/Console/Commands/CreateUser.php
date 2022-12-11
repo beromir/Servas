04f148042358a86e81d25faa6c5b2c11e8b80e4e
Henrik <hreissig@outlook.com>
2022-12-11 13:15:07 +0100

    [FEAT] Add CLI commands to list, delete and create users

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Console\Command;
+use Illuminate\Validation\ValidationException;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Execute the console command.
+     *
+     * @param CreateNewUser $createNewUser
+     * @return int
+     */
+    public function handle(CreateNewUser $createNewUser): int
+    {
+        $name = $this->ask('What is your name?');
+        $email = $this->ask('What is your email?');
+        $password = $this->secret('What is the password?');
+        $confirmPassword = $this->secret('Confirm the password?');
+
+        try {
+            $createNewUser->create([
+                'name' => $name,
+                'email' => $email,
+                'password' => $password,
+                'password_confirmation' => $confirmPassword
+            ]);
+        } catch (ValidationException $exception) {
+            $this->error($exception->getMessage());
+            return Command::FAILURE;
+        }
+
+        $this->info('The user was created!');
+
+        return Command::SUCCESS;
+    }
+}
