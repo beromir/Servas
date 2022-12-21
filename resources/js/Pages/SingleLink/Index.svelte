496a08b9dd6e03d04ba1a238c1f08db108c836aa
Henrik <hreissig@outlook.com>
2022-12-21 21:57:40 +0100

    [WIP] Add "copy link" button to the single link page

--- resources/js/Pages/SingleLink/Index.svelte
+++ resources/js/Pages/SingleLink/Index.svelte
@@ -40,8 +40,17 @@
     <div class="max-w-3xl mx-auto px-4 py-5 bg-white shadow sm:p-6 sm:rounded-lg">
         <div class="md:flex md:flex-row-reverse md:justify-between md:gap-8">
             <div class="flex justify-end md:-mt-1.5 md:h-fit">
+                <button on:click={navigator.clipboard.writeText(link.link)} type="button"
+                        class="w-max inline-flex items-center p-2  border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                              d="M13.887 3.182c.396.037.79.08 1.183.128C16.194 3.45 17 4.414 17 5.517V16.75A2.25 2.25 0 0114.75 19h-9.5A2.25 2.25 0 013 16.75V5.517c0-1.103.806-2.068 1.93-2.207.393-.048.787-.09 1.183-.128A3.001 3.001 0 019 1h2c1.373 0 2.531.923 2.887 2.182zM7.5 4A1.5 1.5 0 019 2.5h2A1.5 1.5 0 0112.5 4v.5h-5V4z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </button>
+
                 <a href={link.link} target="_blank" rel="noreferrer noopener nofollow"
-                   class="w-max inline-flex items-center px-4 py-2  border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+                   class="w-max inline-flex items-center ml-3 px-4 py-2  border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                     Open link
                     <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 -mr-1 h-5 w-5" viewBox="0 0 20 20"
                          fill="currentColor">
