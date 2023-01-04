767803324c347c45d680374e1c5e51b3d2b1a346
Henrik <hreissig@outlook.com>
2023-01-04 19:32:52 +0100

    [TASK] Increase the font weight of the counters

--- resources/js/Pages/Groups/Partials/GroupsGrid.svelte
+++ resources/js/Pages/Groups/Partials/GroupsGrid.svelte
@@ -22,7 +22,7 @@
                                 <path
                                     d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z"/>
                             </svg>
-                            <span class="ml-1 text-gray-600">{group.linksCount}</span>
+                            <span class="ml-1 text-gray-600 font-medium">{group.linksCount}</span>
                         </div>
 
                         <div class="inline-flex ml-3.5">
@@ -31,7 +31,7 @@
                                 <path
                                     d="M5.127 3.502L5.25 3.5h9.5c.041 0 .082 0 .123.002A2.251 2.251 0 0012.75 2h-5.5a2.25 2.25 0 00-2.123 1.502zM1 10.25A2.25 2.25 0 013.25 8h13.5A2.25 2.25 0 0119 10.25v5.5A2.25 2.25 0 0116.75 18H3.25A2.25 2.25 0 011 15.75v-5.5zM3.25 6.5c-.04 0-.082 0-.123.002A2.25 2.25 0 015.25 5h9.5c.98 0 1.814.627 2.123 1.502a3.819 3.819 0 00-.123-.002H3.25z"/>
                             </svg>
-                            <span class="ml-1 text-gray-600">{group.childGroupsCount}</span>
+                            <span class="ml-1 text-gray-600 font-medium">{group.childGroupsCount}</span>
                         </div>
                     </div>
                 </div>
