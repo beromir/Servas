42bbd5218e68c7f394799197c85c686ec8461c43
Yee Jia Wei <yeejiawei0811@gmail.com>
2023-03-14 04:20:13 +0800

    fix component lazy loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,16 @@
 import('./bootstrap');
 import '../css/app.css';
 
-import {createInertiaApp} from '@inertiajs/svelte';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createInertiaApp } from '@inertiajs/svelte';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: title => `${title} - ${appName}`,
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.svelte', {eager: true});
-        return pages[`./Pages/${name}.svelte`];
-    },
-    setup({el, App, props}) {
-        new App({target: el, props});
+    resolve: name => resolvePageComponent(`./Pages/${name}.svelte`, import.meta.glob('./Pages/**/*.svelte')),
+    setup({ el, App, props }) {
+        new App({ target: el, props });
     },
     progress: {
         color: '#4e64b7',
