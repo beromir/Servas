cc1c0c4c4481a37767b833c22c30099d98a8ec26
Nerijus <nerijus.bukartas@gmail.com>
2023-03-29 11:42:42 +0300

    Add demo user & fix seeder

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,6 +20,7 @@ class LinkFactory extends Factory
         return [
             'link' => $this->faker->url(),
             'title' => $this->faker->city(),
+            'user_id' => User::factory(),
         ];
     }
 }
