c6ee9f010cb003033f1d0424ec7b138b1496a5ef
Henrik <hreissig@outlook.com>
2023-09-07 21:30:41 +0200

    [WIP] Redesign tag modal

--- resources/js/Partials/TagSelectMenu.svelte
+++ resources/js/Partials/TagSelectMenu.svelte
@@ -8,7 +8,7 @@
 
     let tags = [];
     let internalSelectedTags = [];       // property for internal use only; contains tag objects
-    let showModal = false;
+    let showModal = true;
 
     const dispatch = createEventDispatcher();
 
@@ -79,37 +79,31 @@
     function getIndexOfTagId(tagId, arrayToSearch) {
         return arrayToSearch.findIndex(tag => tag.id === tagId);
     }
+
+    getAllTags();
 </script>
 
 <Modal title="Select tags" bind:showModal>
     <div class="mt-5">
-        <ul class="mt-3 divide-y divide-gray-100">
+        <div class="grid grid-cols-2 gap-y-4 gap-x-2 mt-3">
             {#each tags as tag (tag.id)}
-                <li class="flex justify-between items-center text-gray-900 cursor-default select-none relative py-2.5 pl-8 pr-4">
-                    <div class="font-normal block truncate">{tag.name}</div>
-
-                    {#if getIndexOfTagId(tag.id, internalSelectedTags) !== -1}
-                        <div class="text-indigo-600 absolute inset-y-0 left-0 flex items-center pl-1.5">
-                            <!-- Heroicon name: solid/check -->
-                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                 fill="currentColor"
-                                 aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                        </div>
-                    {/if}
+                {#if getIndexOfTagId(tag.id, internalSelectedTags) !== -1}
                     <button on:click|stopPropagation={() => selectTag(tag)} type="button"
-                            class="text-sm text-gray-700">
-                        {getIndexOfTagId(tag.id, internalSelectedTags) !== -1 ? 'Unselect' : 'Select'}
+                            class="flex items-center py-3 px-4 border border-primary-200 bg-primary-100 rounded-full hover:border-primary-300 focus:outline-none">
+                        <span class="w-2 h-2 bg-primary-500 rounded-full"></span>
+                        <span class="ml-3 text-sm text-primary-900 truncate">{tag.name}</span>
                     </button>
-                </li>
-
+                {:else}
+                    <button on:click|stopPropagation={() => selectTag(tag)} type="button"
+                            class="flex items-center py-3 px-4 border border-gray-100 bg-gray-50 rounded-full hover:border-gray-200 focus:outline-none">
+                        <span class="w-2 h-2 bg-primary-500 rounded-full"></span>
+                        <span class="ml-3 text-sm text-gray-900 truncate">{tag.name}</span>
+                    </button>
+                {/if}
             {:else}
                 <li class="py-2.5 px-4 text-gray-600 text-center">No tags found</li>
             {/each}
-        </ul>
+        </div>
     </div>
 
     <svelte:fragment slot="footer">
