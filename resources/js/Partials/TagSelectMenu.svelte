a1dc7f6f365f6a21f38c2cecdf78479b440f193c
Henrik <hreissig@outlook.com>
2023-09-08 20:06:01 +0200

    [WIP] Redesign tag modal

--- resources/js/Partials/TagSelectMenu.svelte
+++ resources/js/Partials/TagSelectMenu.svelte
@@ -84,12 +84,12 @@
 </script>
 
 <Modal title="Select tags" bind:showModal>
-    <div class="mt-5">
-        <div class="grid grid-cols-2 gap-y-4 gap-x-2 mt-3">
+    <div class="relative mt-5">
+        <div class="grid grid-cols-2 gap-y-4 gap-x-2 mt-3 pb-10">
             {#each tags as tag (tag.id)}
                 {#if getIndexOfTagId(tag.id, internalSelectedTags) !== -1}
                     <button on:click|stopPropagation={() => selectTag(tag)} type="button"
-                            class="flex items-center py-3 px-4 border border-primary-200 bg-primary-100 rounded-full hover:border-primary-300 focus:outline-none">
+                            class="flex items-center py-3 px-4 border border-primary-100 bg-primary-50 rounded-full hover:border-primary-200 focus:outline-none">
                         <span class="w-2 h-2 bg-primary-500 rounded-full"></span>
                         <span class="ml-3 text-sm text-primary-900 truncate">{tag.name}</span>
                     </button>
@@ -103,6 +103,23 @@
             {:else}
                 <li class="py-2.5 px-4 text-gray-600 text-center">No tags found</li>
             {/each}
+        </div>
+
+        <div class="absolute bottom-0 left-1/2">
+            {#if internalSelectedTags.length}
+                <button on:click={() => internalSelectedTags = []} type="button"
+                        class="absolute bottom-0 left-0 flex items-center w-max text-gray-700 -translate-x-1/2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                         class="w-5 h-5 fill-gray-500">
+                        <path fill-rule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                    <span class="ml-1.5">
+                        Deselect all
+                    </span>
+                </button>
+            {/if}
         </div>
     </div>
 
