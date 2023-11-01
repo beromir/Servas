55b7de2bb32484dfd4b8d7122e59424b85ef3944
Henrik <hreissig@outlook.com>
2023-11-01 18:47:37 +0100

    [TASK] Do not stop event propagation when clicking outside

--- resources/js/utils/index.js
+++ resources/js/utils/index.js
@@ -19,7 +19,6 @@ export function clickOutside(node) {
 
     const handleClick = event => {
         if (node && !node.contains(event.target) && !event.defaultPrevented) {
-            event.stopPropagation();
             node.dispatchEvent(
                 new CustomEvent('click_outside', node)
             )
