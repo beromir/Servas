097eee0bad8e4aace5f160eab2baae7d423bc320
Henrik <hreissig@outlook.com>
2023-12-11 19:57:18 +0100

    [TASK] Revise link list filter design on mobile

--- resources/js/Layouts/AppLayout/Partials/Main.svelte
+++ resources/js/Layouts/AppLayout/Partials/Main.svelte
@@ -7,16 +7,16 @@
 
 <div class="max-w-7xl mx-auto pt-4 pb-12 sm:px-6 lg:px-8">
 
-    <div class="px-4 pb-4 w-full sm:flex justify-between border-b-2 sm:px-0"
+    <div class="flex px-4 pb-4 w-full justify-between items-center border-b-2 sm:px-0"
          class:border-transparent={!title} class:border-gray-200={title}>
         {#if title}
-            <h1 class="text-3xl text-gray-900 font-bold">{title}</h1>
+            <h1 class="text-2xl text-gray-900 font-bold md:text-3xl">{title}</h1>
         {/if}
         <slot name="toolbar">
             <div></div>
         </slot>
 
-        <div class="hidden space-x-3 sm:inline-flex">
+        <div class="hidden space-x-3 sm:inline-flex sm:ml-6">
             <Button on:clicked="{() => dispatchCustomEvent('prepareCreateNewLink')}" title="new link"
                     class="focus:ring-offset-gray-100">
                 <svg slot="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
--- resources/js/Pages/SingleGroup/Index.svelte
+++ resources/js/Pages/SingleGroup/Index.svelte
@@ -36,7 +36,7 @@
 
 <Main title={group.title}>
     <svelte:fragment slot="toolbar">
-        <div class="relative inline-flex mr-6 ml-auto">
+        <div class="relative inline-flex ml-auto">
             <button on:click={() => showMenuDropdown = !showMenuDropdown} type="button"
                     class="text-gray-600 hover:text-gray-900">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -45,12 +45,15 @@
                           d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
                 </svg>
             </button>
-            <Dropdown bind:showDropdown={showMenuDropdown} class="top-full !mt-0 !w-44 !origin-top-right">
+            <Dropdown bind:showDropdown={showMenuDropdown} openingDirection="left"
+                      class="top-full !mt-0 !w-44 !origin-top-right">
                 <InnerDropdownSection>
                     <DropdownItem on:clicked={() => handleGroupMenuAction('editGroup')} title="Edit">
                         <svg slot="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"/>
-                            <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"/>
+                            <path
+                                d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"/>
+                            <path
+                                d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"/>
                         </svg>
                     </DropdownItem>
                     <DropdownItem on:clicked={() => handleGroupMenuAction('deleteGroup')} title="Delete"
@@ -66,10 +69,7 @@
         </div>
     </svelte:fragment>
 
-    <div class="mt-6">
-
-        {#if links.length !== 0}
-            <LinkList {links} {searchString} {filteredTags} {showUntaggedOnly}/>
-        {/if}
-    </div>
+    {#if links.length !== 0}
+        <LinkList {links} {searchString} {filteredTags} {showUntaggedOnly}/>
+    {/if}
 </Main>
