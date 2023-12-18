804bf9ed7a8379d1ac0d6786689bcb989b38aac0
Henrik <hreissig@outlook.com>
2023-12-18 21:01:40 +0100

    [TASK] Make mobile sidebar toggle round

--- resources/js/Layouts/AppLayout/AppLayout.svelte
+++ resources/js/Layouts/AppLayout/AppLayout.svelte
@@ -89,7 +89,7 @@
     {/if}
 
     <button on:click={() => showSidebar = toggleSidebar()} type="button"
-            class="fixed bottom-0 left-4 z-10 mb-4 py-1.5 px-2 text-gray-100 bg-gray-800 rounded-md shadow sm:hidden">
+            class="fixed bottom-4 left-4 z-10 p-2 text-gray-100 bg-gray-800 rounded-full shadow sm:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
              class="w-8 h-8">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
