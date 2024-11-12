517ef5c79ef094256dd7c01cf3f5385883a0734f
Henrik <hreissig@outlook.com>
2024-11-12 17:50:36 +0100

    [TASK] Use button component for sharing button

--- resources/js/Pages/SingleGroup/Index.svelte
+++ resources/js/Pages/SingleGroup/Index.svelte
@@ -112,15 +112,14 @@
         </div>
 
         {#if publicLink.id}
-            <button on:click={() => showPublicLinkModal = true} type="button" title="Public link"
-                    class="ml-4 py-2 px-2 bg-gray-50 shadow-sm rounded-md ring-1 ring-gray-200 md:bg-white md:hover:bg-gray-50">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="size-5 fill-gray-800">
+            <Button on:clicked={() => showPublicLinkModal = true} color="white"  class="ml-4">
+                <svg slot="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path
                         d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z"/>
                     <path
                         d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z"/>
                 </svg>
-            </button>
+            </Button>
         {/if}
     </svelte:fragment>
 
