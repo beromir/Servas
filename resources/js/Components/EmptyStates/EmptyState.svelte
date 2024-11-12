de56feed2f7311a9c3233fdf511a89a770d7d21b
Henrik <hreissig@outlook.com>
2024-11-12 21:11:02 +0100

    [TASK] Add dark mode to public link page

--- resources/js/Components/EmptyStates/EmptyState.svelte
+++ resources/js/Components/EmptyStates/EmptyState.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <svelte:element this={tag}
-                class="flex flex-col items-center col-span-full gap-y-1 text-sm text-gray-600 font-medium md:text-base">
+                class="flex flex-col items-center col-span-full gap-y-1 text-sm text-gray-700 font-medium md:text-base dark:text-gray-200">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-         stroke="currentColor" class="size-8 text-gray-400 md:size-10">
+         stroke="currentColor" class="size-8 text-gray-500 md:size-10 dark:text-gray-300">
         <path stroke-linecap="round" stroke-linejoin="round"
               d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
     </svg>
