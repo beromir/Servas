cccd185df682ce1d581fff9e95da26852bd6f6c3
Henrik <hreissig@outlook.com>
2024-11-12 18:20:13 +0100

    [TASK] Add dark mode to single group page

--- resources/js/Components/Buttons/Button.svelte
+++ resources/js/Components/Buttons/Button.svelte
@@ -7,23 +7,25 @@
     export let type = 'button';
     export let title = null;
     export let color = undefined;
+    export let hoverTitle = '';
     export let focusButton = false;
 
     function getColors() {
         switch (color) {
             case 'red':
-                return 'border-transparent text-white bg-red-600 hover:bg-red-700 focus:ring-red-500';
+                return clsx(
+                    'border-transparent text-white bg-red-600 hover:bg-red-700 focus:ring-red-500',
+                    '[&>svg]:fill-white dark:[&>svg]:fill-gray-950',
+                );
             case 'white':
                 return clsx(
                     'border-gray-400 text-gray-700 bg-white hover:bg-gray-50 focus:ring-primary-400 dark:bg-gray-800 dark:text-gray-100 dark:border-gray-600 dark:hover:bg-gray-700',
                     '[&>svg]:fill-gray-500 dark:[&>svg]:fill-gray-300',
-                    title ? '[&>svg]:-ml-1 [&>svg]:mr-2' : '',
                 );
             default:
                 return clsx(
                     'border-transparent text-white bg-primary-500 hover:bg-primary-600 focus:ring-primary-400',
                     '[&>svg]:fill-gray-100',
-                    title ? '[&>svg]:-ml-1 [&>svg]:mr-2' : '',
                 );
         }
     }
@@ -35,11 +37,12 @@
     class={clsx(
         'w-full inline-flex justify-center items-center py-2 border shadow-sm text-sm font-medium rounded-md select-none focus:outline-none focus:ring-2 focus:ring-offset-2 md:w-auto dark:focus:ring-offset-gray-900',
         '[&>svg]:size-5',
-        title ? 'px-4' : 'px-2',
+        title ? 'px-4 [&>svg]:-ml-1 [&>svg]:mr-2' : 'px-2',
         getColors(),
         $$restProps.class
     )}
-    use:focusSelf={focusButton}>
+    use:focusSelf={focusButton}
+    title={hoverTitle}>
     {#if $$slots.icon}
         <slot name="icon"/>
     {/if}
