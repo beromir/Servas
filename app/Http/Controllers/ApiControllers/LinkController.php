1a32b8bdb5fe41e22ea2819ba381fd8cd4e70afe
Henrik <hreissig@outlook.com>
2024-11-27 21:02:58 +0100

    [TASK] Make it possible to tag and group link via API when storing

--- app/Http/Controllers/ApiControllers/LinkController.php
+++ app/Http/Controllers/ApiControllers/LinkController.php
@@ -6,11 +6,20 @@ use App\Helpers\WebpageData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreLinkApiRequest;
 use App\Models\Link;
+use App\Services\Models\GroupService;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Tags\Tag;
 
 class LinkController extends Controller
 {
+    public function __construct(
+        protected GroupService $groupService,
+    )
+    {
+        //
+    }
+
     /**
      * Store a newly created resource in storage.
      */
@@ -29,6 +38,24 @@ class LinkController extends Controller
         }
 
         $link->save();
+
+        if (key_exists('groups', $validated)) {
+            $groupIds = $validated['groups'];
+
+            $link->groups()->sync($groupIds);
+
+            $this->groupService->updateUserGroupsLinkCount(Auth::user());
+        }
+
+        if (key_exists('tags', $validated)) {
+            $tags = [];
+
+            foreach ($validated['tags'] as $tag) {
+                $tags[] = Tag::filterByCurrentUser()->find($tag);
+            }
+
+            $link->syncTags($tags);
+        }
 
         return response('The link was added.', headers: ['Content-Type' => 'text/plain']);
     }
