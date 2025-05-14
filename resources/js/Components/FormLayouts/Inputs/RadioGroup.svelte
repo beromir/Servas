4751cdba37fb27dbe9b52d50bd167223ca127ad9
Henrik <hreissig@outlook.com>
2025-05-14 21:18:22 +0200

    [TASK] Add radio group components

---
+++ resources/js/Components/FormLayouts/Inputs/RadioGroup.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let label;
+</script>
+
+<fieldset>
+    <legend class="font-medium text-sm text-gray-700 dark:text-gray-200">{label}</legend>
+    <div class="mt-2 space-y-1">
+        <slot/>
+    </div>
+</fieldset>
