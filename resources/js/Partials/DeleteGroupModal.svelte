dbc9ef6b7180581a00f7ce868f27feafc014cad0
Henrik <hreissig@outlook.com>
2025-11-07 19:55:44 +0100

    [FIX] Only render modal content if not undefined

--- resources/js/Partials/DeleteGroupModal.svelte
+++ resources/js/Partials/DeleteGroupModal.svelte
@@ -27,21 +27,21 @@
 
 <svelte:window ondeleteGroup={prepareDeleteGroup}/>
 
-{#if group}
-    <Modal bind:this={modal} title="Delete Group">
+<Modal bind:this={modal} title="Delete Group">
+    {#if group}
         <p class="text-sm text-gray-600 dark:text-gray-300">
             Are you sure you want to delete this group?
         </p>
         <div class="mt-1 text-sm text-gray-700 font-medium dark:text-gray-50">
             {group.title}
         </div>
+    {/if}
 
-        {#snippet footer()}
+    {#snippet footer()}
 
-            <Button clicked={() => modal.close()} title="Cancel" color="white"
-                    class="hidden sm:block"/>
-            <Button clicked={deleteGroup} title="Delete" color="red"/>
+        <Button clicked={() => modal.close()} title="Cancel" color="white"
+                class="hidden sm:block"/>
+        <Button clicked={deleteGroup} title="Delete" color="red"/>
 
-        {/snippet}
-    </Modal>
-{/if}
+    {/snippet}
+</Modal>
--- resources/js/Partials/DeleteLinkModal.svelte
+++ resources/js/Partials/DeleteLinkModal.svelte
@@ -27,19 +27,19 @@
 
 <svelte:window ondeleteLink={prepareDeleteLink}/>
 
-{#if link}
-    <Modal bind:this={modal} title="Delete Link">
+<Modal bind:this={modal} title="Delete Link">
+    {#if link}
         <p class="text-sm text-gray-600 dark:text-gray-300">
             Are you sure you want to delete this link?
         </p>
         <div class="mt-1 text-sm text-gray-700 font-medium dark:text-gray-50">
             {link.title}
         </div>
+    {/if}
 
-        {#snippet footer()}
-            <Button clicked={() => modal.close()} title="Cancel" color="white"
-                    class="hidden sm:block"/>
-            <Button clicked={deleteLink} title="Delete" color="red"/>
-        {/snippet}
-    </Modal>
-{/if}
+    {#snippet footer()}
+        <Button clicked={() => modal.close()} title="Cancel" color="white"
+                class="hidden sm:block"/>
+        <Button clicked={deleteLink} title="Delete" color="red"/>
+    {/snippet}
+</Modal>
