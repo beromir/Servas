5ac830f2c03fe140a422286d1da519244559a5a6
Henrik <hreissig@outlook.com>
2025-11-09 14:26:01 +0100

    [TASK] Remove unused components

--- resources/js/Jetstream/Checkbox.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import clsx from "clsx";
-
-    let {checked = $bindable(), ...props} = $props();
-</script>
-
-<input type="checkbox" bind:checked {...props}
-       class={clsx('rounded-sm border-gray-300 text-primary-500 shadow-xs', props.class)}>
