4f18ecfba76583a3c95ca2157d8d12fc402a8bbe
Henrik <hreissig@outlook.com>
2025-11-14 23:59:42 +0100

    [Task] Use new input component

---
+++ resources/js/Components/Input.svelte
@@ -0,0 +1,12 @@
+<script>
+    import clsx from "clsx";
+
+    let {
+        type = 'text',
+        value = $bindable(null),
+        ...props
+    } = $props();
+</script>
+
+<input {type} bind:value {...props}
+       class={clsx('block w-full rounded-md border-gray-400 shadow-xs focus:border-primary-500 focus:ring-primary-500 sm:text-sm dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400', props.class)}>
