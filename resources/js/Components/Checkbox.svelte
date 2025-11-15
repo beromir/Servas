7720af65bbd0aa3da70a8b50ce32eebf481e1ec3
Henrik <hreissig@outlook.com>
2025-11-15 17:49:57 +0100

    [TASK] Adjust checkbox and radio button styling

--- resources/js/Components/Checkbox.svelte
+++ resources/js/Components/Checkbox.svelte
@@ -3,4 +3,4 @@
 </script>
 
 <input type="checkbox" bind:checked {...props}
-       class="size-4 rounded-sm border-gray-300 ring-0! ring-offset-0! focus:outline-2 focus:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-900 dark:border-gray-500">
+       class="size-4 rounded-sm border-gray-400 ring-0! ring-offset-0! focus:outline-2 focus:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-950 dark:border-gray-500">
--- resources/js/Components/FormLayouts/Inputs/Radio.svelte
+++ resources/js/Components/FormLayouts/Inputs/Radio.svelte
@@ -12,6 +12,6 @@
 <div class="flex items-center">
     <input type="radio" onchange={(event) => currentValue = event.target.checked ? value : currentValue}
            {id} {name} {value} checked={value === currentValue} {...rest}
-           class="text-primary-600  dark:bg-gray-800 dark:border-gray-600">
+           class="border-gray-400 ring-0! ring-offset-0! focus:outline-2 focus:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-950 dark:border-gray-500">
     <label for={id} class="block ml-2 text-sm text-gray-600 capitalize dark:text-gray-300">{label}</label>
 </div>
