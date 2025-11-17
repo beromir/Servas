e2f95abc3db2b2a47ca4f8c4fc620975d5d9c958
Henrik <hreissig@outlook.com>
2025-11-17 18:50:47 +0100

    [TASK] Apply focus styles when using keyboard only

--- resources/js/Components/Checkbox.svelte
+++ resources/js/Components/Checkbox.svelte
@@ -3,4 +3,4 @@
 </script>
 
 <input type="checkbox" bind:checked {...props}
-       class="size-4 rounded-sm border-gray-400 ring-0! ring-offset-0! focus:outline-2 focus:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-950 dark:border-gray-500">
+       class="size-4 rounded-sm border-gray-400 ring-0! ring-offset-0! focus-visible:outline-2 focus-visible:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-950 dark:border-gray-500">
--- resources/js/Components/FormLayouts/Inputs/Radio.svelte
+++ resources/js/Components/FormLayouts/Inputs/Radio.svelte
@@ -12,6 +12,6 @@
 <div class="flex items-center">
     <input type="radio" onchange={(event) => currentValue = event.target.checked ? value : currentValue}
            {id} {name} {value} checked={value === currentValue} {...rest}
-           class="border-gray-400 ring-0! ring-offset-0! focus:outline-2 focus:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-950 dark:border-gray-500">
+           class="border-gray-400 ring-0! ring-offset-0! focus-visible:outline-2 focus-visible:outline-primary-500 checked:bg-primary-600 checked:border-primary-600 dark:bg-gray-950 dark:border-gray-500">
     <label for={id} class="block ml-2 text-sm text-gray-600 capitalize dark:text-gray-300">{label}</label>
 </div>
